e73218d01b551d41a643fb3e35da1599a7ca5b60
xuejike <xuejike2004@126.com>
2017-07-31 13:02:36 +0800

    新增Http 服务工具

--- printWin/Form1.Designer.cs
+++ printWin/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(75, 66);
+            this.button1.Location = new System.Drawing.Point(81, 68);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
---
+++ printWin/HttpTool.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace printWin
+{
+    class HttpTool
+    {
+        public static void Listen(string hostPort, HttpCallBack iHttpCallback)
+        {
+            HttpListener httpListener = new HttpListener();
+
+            httpListener.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
+            httpListener.Prefixes.Add(hostPort);
+            httpListener.Start();
+            new Thread(new ThreadStart(delegate
+            {
+                while (true)
+                {
+                    HttpListenerContext httpListenerContext = httpListener.GetContext();
+
+                    Stream reqStream = httpListenerContext.Request.InputStream;
+                    NameValueCollection queryString = httpListenerContext.Request.QueryString;
+                    string body = new StreamReader(reqStream).ReadToEnd();
+
+                    object result = iHttpCallback(queryString, body);
+                    httpListenerContext.Response.StatusCode = 200;
+                    httpListenerContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                    httpListenerContext.Response.Headers.Add("Content-Type", "text/json; charset=utf-8");
+                    using (StreamWriter writer = new StreamWriter(httpListenerContext.Response.OutputStream))
+                    {
+                        string json = JsonConvert.SerializeObject(result);
+                        writer.WriteLine(json);
+                    }
+                }
+            })).Start();
+        }
+    }
+
+    delegate Object HttpCallBack(NameValueCollection queryString, String body);
+}
